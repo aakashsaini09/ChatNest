8084eabd28cd46ddcb50ff4131e673841e8e2469
aakashsaini09 <aakashsaini948585@gmail.com>
2024-09-30 17:48:09 +0530

    routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,21 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
+import Auth from './pages/auth'
+import Chat from './pages/chat'
+import Profile from './pages/profile'
 
 function App() {
 
   return (
     <>
-      <p className="read-the-docs text-4xl font-bold text-red-900 min-h-[100vh] min-w-[100vw] bg-gray-800">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+     <BrowserRouter>
+     <Routes>
+      <Route path='/auth' element={<Auth/>}/>
+      <Route path="/chat" element={<Chat/>}/>
+      <Route path="/profile" element={<Profile/>}/>
+     </Routes>
+     </BrowserRouter>
+     </>
   )
 }
 
