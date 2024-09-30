5495422778397e9b6bf4c10702ccb8294952078f
aakashsaini09 <aakashsaini948585@gmail.com>
2024-09-30 17:32:49 +0530

    structure

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,7 +4,7 @@ function App() {
 
   return (
     <>
-      <p className="read-the-docs">
+      <p className="read-the-docs text-4xl font-bold text-red-900 min-h-[100vh] min-w-[100vw] bg-gray-800">
         Click on the Vite and React logos to learn more
       </p>
     </>
