f41c350558e6bafcde802300e6e5f5100f3a0a2c
aakashsaini09 <aakashsaini948585@gmail.com>
2024-10-02 21:14:35 +0530

    schema

--- server/index.js
+++ server/index.js
@@ -1,1 +1,22 @@
-const express = require('express')
+import express from 'express'
+import dotenv from "dotenv";
+import cors from "cors"
+import cookieParser from 'cookie-parser';
+import mongoose from 'mongoose';
+
+dotenv.config();
+const app = express();
+app.use(cors({
+    origin:[process.env.ORIGIN],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    credentials: true
+}))
+app.use(cookieParser());
+app.use(express.json());
+const port = process.env.PORT || 3000;
+const dbURL = process.env.DATABASE_URL
+const server = app.listen(port, () => {
+    console.log(`Server is running at port: ${port}`)
+})
+
+mongoose.connect(dbURL).then(() => console.log("db connection Successfull"))
