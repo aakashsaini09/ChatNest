b338716faf52239e6bcbfe5bcda3cba8f7049fff
aakashsaini09 <aakashsaini948585@gmail.com>
2024-11-23 16:57:20 +0530

    redux store

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -4,12 +4,16 @@ import './index.css'
 import App from './App.tsx'
 import { RouterProvider } from 'react-router-dom'
 import router from './routes/index.tsx'
+import { Provider } from 'react-redux'
+import { store } from './redux/store.ts'
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-      {/* @ts-ignore */}
-      <RouterProvider router={router}>
-      <App />
-      </RouterProvider>
+      <Provider store={store}>
+        {/* @ts-ignore */}
+        <RouterProvider router={router}>
+          <App />
+        </RouterProvider>
+      </Provider>
     </StrictMode>
 )
