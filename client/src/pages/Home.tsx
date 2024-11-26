e2f13f2a99726ccf952b997f169c5eee984efa44
aakashsaini09 <aakashsaini948585@gmail.com>
2024-11-26 17:06:45 +0530

    fix

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from "react-redux"
-import { Outlet, useNavigate } from "react-router-dom"
+import { Outlet, useLocation, useNavigate } from "react-router-dom"
+import logo from '../assets/img.jpg'
 import { RootState } from '../redux/store';
 import axios from "axios";
 import toast from "react-hot-toast";
@@ -8,6 +9,7 @@ import { logout, setUser } from "../redux/userSlice";
 import Sidebar from "../components/Sidebar";
 const Home = () => {
   const url = `${import.meta.env.VITE_APP_BACKEND_URL}/user-details`
+  const loaction = useLocation()
   const navigate = useNavigate()
   const dispatch = useDispatch()
   useSelector((state: RootState) => state.user);
@@ -17,7 +19,7 @@ const Home = () => {
         url: url,
         withCredentials: true
       });
-      if(res.data.logout){
+      if(res.data.data.logout){
         dispatch(logout({}))
         navigate('/email')
       }
@@ -31,10 +33,17 @@ const Home = () => {
   useEffect(() => {
     getUserInfo()
   }, [])
+  const basePath = loaction.pathname === '/'
   return (
     <div className="grid grid-cols-[300px,1fr] h-screen max-h-screen">
-      <section className="bg-white"><Sidebar/></section>
-      <section><Outlet/></section>
+      <section className={`bg-white ${!basePath && 'hidden'} lg:block`}><Sidebar/></section>
+      <section className={`${basePath && 'hidden'}`}><Outlet/></section>
+      <div className="lg:flex justify-center items-center flex-col gap-2 hidden">
+        <div>
+          <img src={logo} width={250} alt="" />
+        </div>
+        <p className="text-lg mt-2 text-slate-600">Send Message to Someone</p>
+      </div>
     </div>
   )
 }
