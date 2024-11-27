0bd453c7f21a3545ded311e040149cc40bd9e61c
aakashsaini09 <aakashsaini948585@gmail.com>
2024-11-27 16:44:30 +0530

    searchUser

--- client/src/components/Sidebar.tsx
+++ client/src/components/Sidebar.tsx
@@ -7,18 +7,22 @@ import { useSelector } from "react-redux"
 import { RootState } from "../redux/store"
 import { useState } from "react"
 import EditUserPopup from "./EditUserPopup"
+import { FiArrowUpLeft } from "react-icons/fi"
+import SearchUser from "./SearchUser"
 const Sidebar = () => {
   const user = useSelector((state: RootState) => state.user);
   const [editUser, seteditUser] = useState(false)
+  const [allUsers, setallUsers] = useState([])
+  const [openSearchUser, setopenSearchUser] = useState(true)
   return (
     <>
-      <div className="w-full h-full">
+      <div className="w-full h-full grid grid-cols-[48px,1fr] bg-white">
         <div className="bg-slate-200 w-12 h-full rounded-tr-lg rounded-br-lg py-5 text-slate-600 flex flex-col justify-between">
             <div className="">
                 <NavLink to={''} className={({isActive})=>`w-12 h-12 flex justify-center items-center cursor-pointer hover:bg-slate-300 rounded ${isActive && 'bg-slate-300'}`} title="chat">
                     <IoChatbubbleEllipses size={22}/>
                 </NavLink>
-                <div className="w-12 h-12 flex justify-center items-center cursor-pointer hover:bg-slate-300 rounded" title="chat">
+                <div onClick={() => setopenSearchUser(true)} className="w-12 h-12 flex justify-center items-center cursor-pointer hover:bg-slate-300 rounded" title="Add Friend">
                     <FaUserPlus size={22}/>
                 </div>
             </div>
@@ -33,8 +37,28 @@ const Sidebar = () => {
                 </button>
             </div>
         </div>
+        <div className="w-full">
+          <div className="h-16 flex items-center">
+            <h2 className="text-xl font-bold p-4 text-slate-800">Message</h2> 
+          </div>
+          <hr />
+          <div className="h-[calc(100vh-65px)] overflow-x-hidden overflow-y-auto scrollbar">
+            {allUsers.length === 0 && (
+              <div className="mt-12">
+                <div className="flex justify-center items-center my-4 text-slate-500">
+                  <FiArrowUpLeft size={50}/>
+                </div>
+                <p className="text-lg text-center text-slate-400">Explore Users to start a conversation.</p>
+              </div>
+            )}
+          </div>
+        </div>
         {editUser && (
           <EditUserPopup data={user} onclose={()=>seteditUser(false)}/>
+        )}
+        {/* Search User */}
+        {openSearchUser && (
+          <SearchUser onClose={()=> setopenSearchUser(false)}/>
         )}
       </div>
     </>
