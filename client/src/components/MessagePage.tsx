1c13e40c619e984572d25b6158f32aef9d8f001d
aakashsaini09 <aakashsaini948585@gmail.com>
2024-11-29 14:45:49 +0530

    header section

--- client/src/components/MessagePage.tsx
+++ client/src/components/MessagePage.tsx
@@ -1,21 +1,61 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useSelector } from "react-redux"
-import { useParams } from "react-router-dom"
+import { Link, useParams } from "react-router-dom"
 import { RootState } from "../redux/store"
+import Avatar from "./Avatar"
+import { HiDotsVertical } from "react-icons/hi"
+import { FaAngleLeft } from "react-icons/fa"
 
 const MessagePage = () => {
   const params = useParams()
+  const user = useSelector((state: RootState) => state.user)
+  const [userData, setuserData] = useState({
+    _id: '',
+    name: '',
+    email: '',
+    profile_pic: '',
+    online: false
+  })
   const socketConnection = useSelector((state: RootState )=> state?.user?.socketConnection)
-  console.log("params: ", params.userId)
+  console.log("socketConnection: ", socketConnection)
   useEffect(() => {
     if(socketConnection){
       socketConnection.emit('message-page', params.userId)
+      socketConnection.on('message-user', (data: any) => {
+        console.log("data: ", data)
+        setuserData(data)
+      })
     }
-  }, [socketConnection])
+  }, [socketConnection, params?.userId, user])
   
   return (
     <>
-      MessagePage
+      <div>
+        <header className="static top-0 h-16 bg-white flex justify-between items-center px-4">
+          <div className="flex items-center gap-4">
+            <Link to={'/'} className="lg:hidden">
+              <FaAngleLeft size={25}/>
+            </Link>
+            <div>
+              <Avatar 
+                width={50}
+                height={50}
+                profile_pic={userData.profile_pic}
+                name={userData.name}
+                userId={userData._id}
+              />
+            </div>
+            <div>
+              <h3 className="font-semibold text-lg my0">{userData?.name}</h3>
+              <p className="-my-2 text-xs">{userData.online ? <span className="text-green-500">Online</span> : <span className="text-slate-600">Offline</span>}</p>
+            </div>
+          </div>
+          <div >
+            <button className="cursor-pointer hover:text-primary"><HiDotsVertical/></button>
+          </div>
+        </header>
+        <section className="h-[calc(100vh-64px)] bg-purple-600"></section>
+      </div>
     </>
   )
 }
--- server/src/socket/index.js
+++ server/src/socket/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import { Server} from 'socket.io'
 import http from 'http'
 import { extractUserInfo } from '../helper/ExtractToken.js'
+import UserModel from '../schema/UserSchema.js'
 const app = express()
 
 const server = http.createServer(app)
@@ -19,10 +20,19 @@ io.on('connection', async(socket) => {
     const user =  await extractUserInfo(token)
     // Create a room
     socket.join(user?._id)
-    onlineUser.add(user?._id)
+    onlineUser.add(user?._id?.toString())
     io.emit('onlineuser', Array.from(onlineUser))
-    socket.on('message-page', (userId)=>{
+    socket.on('message-page', async (userId)=>{
         console.log("userId is: ", userId)
+        const userDetails = await UserModel.findById(userId).select("-password")
+        const payload = {
+            _id: userDetails._id,
+            name: userDetails.name,
+            email: userDetails.email,
+            profile_pic: userDetails.profile_pic,
+            online: onlineUser.has(userId)
+        }
+        socket.emit('message-user', payload)
     })
     socket.on('disconnect', () => {
         onlineUser.delete(user?._id)
