ea100419572d92f167f332b3e209c4391bd28a33
aakashsaini09 <aakashsaini948585@gmail.com>
2024-11-29 17:27:38 +0530

    upload vid&Img

--- client/src/components/MessagePage.tsx
+++ client/src/components/MessagePage.tsx
@@ -8,9 +8,12 @@ import { FaAngleLeft } from "react-icons/fa"
 import { FaPlus } from "react-icons/fa"
 import { FaImage, FaVideo } from "react-icons/fa6"
 import uploadFile from "../helpers/uploadFiles"
+import { IoClose } from "react-icons/io5"
+import Loading from "./Loading"
 
 const MessagePage = () => {
   const [openImgVidUpload, setopenImgVidUpload] = useState(false)
+  const [loading, setloading] = useState(false)
   const [message, setmessage] = useState({
     text: '',
     imageUrl: '',
@@ -39,30 +42,44 @@ const MessagePage = () => {
     setopenImgVidUpload(pre => !pre)
   }
   const handleUploadImg = async(e:any) => {
-    const file = e.target.files[0]
-
-        const uploadPhoto = await uploadFile(file)
+    setloading(true)
+    const uploadPhoto = await uploadFile(e.target.files[0])
+    console.log("uploadImg is: ", uploadPhoto)
+    setopenImgVidUpload(false)
+    setmessage(pre => {
+      return{
+        ...pre, imageUrl: uploadPhoto?.url
+      }
+    })
+    setloading(false)
+  }
+  const handleClearUploadImg = () => {
         setmessage(pre => {
           return{
-            ...pre, imageUrl: uploadPhoto
+            ...pre, imageUrl: ""
           }
         })
-        // setuserData((preve)=>{
-        // return{
-        //     ...preve,
-        //     profile_pic : uploadPhoto?.url
-        // }
-        // })
+  }
+  const handleClearUploadVid = () => {
+        setmessage(pre => {
+          return{
+            ...pre, videoUrl: ""
+          }
+        })
   }
   const handleUploadVid = async(e: any) => {
-    const file = e.target.files[0]
-
-        const uploadVideo = await uploadFile(file)
-        setmessage(pre => {
-          return{
-            ...pre, videoUrl: uploadVideo
-          }
-        })
+    setloading(true)
+    // const file = e.target.files[0]
+    const uploadVideo = await uploadFile(e.target.files[0])
+    setloading(false)
+    console.log("uploadVideo:", uploadVideo)
+    setopenImgVidUpload(false)
+    setmessage(pre => {
+      return{
+        ...pre, videoUrl: uploadVideo
+      }
+    })
+    setloading(false)
   }
 
   return (
@@ -91,7 +108,40 @@ const MessagePage = () => {
             <button className="cursor-pointer hover:text-primary"><HiDotsVertical/></button>
           </div>
         </header>
-        <section className="h-[calc(100vh-128px)] overflow-x-hidden overflow-y-scroll scrollbar">
+        {/* show msg here */}
+        <section className="h-[calc(100vh-128px)] overflow-x-hidden overflow-y-scroll scrollbar relative">
+          {message.imageUrl && (
+            <div className="w-full h-full bg-slate-700 bg-opacity-30 flex justify-center items-center rounded overflow-hidden">
+            <div onClick={handleClearUploadImg} className="w-fit p-2 absolute top-0 right-0 cursor-pointer hover:text-red-800">
+              <IoClose size={30}/>
+            </div>
+            <div className="bg-white p-3">
+              <img 
+                src={message.imageUrl} 
+                className="aspect-square object-scale-down w-full h-full max-w-sm m-2" 
+                alt="Upload Image" />
+            </div>
+          </div>
+          )}
+          {message.videoUrl && (
+            <div className="w-full h-full bg-slate-700 bg-opacity-30 flex justify-center items-center rounded overflow-hidden">
+            <div onClick={handleClearUploadVid} className="w-fit p-2 absolute top-0 right-0 cursor-pointer hover:text-red-800">
+              <IoClose size={30}/>
+            </div>
+            <div className="bg-white p-3">
+              <video
+                src={message.videoUrl} 
+                className="aspect-square object-scale-down w-full h-full max-w-sm m-2"
+                controls
+                muted 
+                autoPlay
+              />
+            </div>
+          </div>
+          )}
+          {loading && (
+            <div className="w-full h-full flex justify-center items-center"><Loading/></div>
+          )}
           All messages here
         </section>
         <section className="h-16 bg-white flex items-center px-4">
@@ -115,8 +165,8 @@ const MessagePage = () => {
                   </div>
                   <p>Video</p>
                 </label>
-                <input onClick={handleUploadImg} type="file" id="uploadVideo" />
-                <input onClick={handleUploadVid} type="file" id="uploadVideo" />
+                <input className="hidden" onClick={handleUploadImg} type="file" id="uploadImage" />
+                <input className="hidden" onClick={handleUploadVid} type="file" id="uploadVideo" />
               </form>
             </div>
             }
